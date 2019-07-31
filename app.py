e024f265c647195206f9910d6a9588b4845fd8c4
myname21-meet <myname21@meet.mit.edu>
2019-07-31 04:26:26 -0400

    uploded lab into herouku

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from databases import *
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home(): 
+	return render_template("home.html")
+
+@app.route('/student/<int:student_id>')
+def display_student(student_id):
+	variable = query_by_id(student_id)
+	return render_template("student.html",id_number = student_id, student = variable)
+
+	
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+	app.run(debug=True, port=8080)
